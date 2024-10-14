e13a3c5465a4d9c705125430af00d2aa019686da
Lenok <eseredich8@gmail.com>
2024-10-14 23:37:55 +0300

    feat: step 16

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,10 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
-    "use strict";
+    'use strict';
 
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 });
